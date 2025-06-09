b429c5692d1ac74dd60110559613c42a70d48472
BP15 <lawhitenton62@gmail.com>
2025-06-09 13:01:22 -0500

    Create PluginInfo.cs

---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GorillaPlayerIDDisplay
+{
+    public class PluginInfo
+    {
+        public const string GUID = "com.BP15.GorillaPlayerIDDisplay";
+        public const string Name = "GorillaPlayerIDDisplay";
+        public const string Version = "1.0.0";
+    }
+}
